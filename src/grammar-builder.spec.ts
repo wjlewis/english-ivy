f08960534704d7ded97bbc1513c1dc8f5056373e
wjl <william.lewis112358@gmail.com>
2020-12-24 08:44:38 -0500

    Add test for `GrammarBuilder`

---
+++ src/grammar-builder.spec.ts
@@ -0,0 +1,64 @@
+import { GrammarBuilder as Gb } from './grammar-builder';
+import { ExpansionKind } from './types/grammar';
+
+describe('GrammarBuilder', () => {
+  it('Builds the expected grammar', () => {
+    const gram = new Gb()
+      .addProduction('Expr', Gb.Alt('Atom', 'Sum', 'Prod'))
+      .addProduction('Atom', Gb.Alt('Name', 'Num'))
+      .addProduction('Sum', Gb.Seq('Expr', 'Expr'))
+      .addProduction('Prod', Gb.Seq('Expr', 'Expr'))
+      .addProduction('Name', Gb.Terminal(/^\w[\w\d]*$/))
+      .addProduction('Num', Gb.Terminal(/^\d+$/))
+      .startWith('Expr')
+      .build();
+
+    expect(gram).toEqual({
+      entry: 'Expr',
+      productions: {
+        Expr: {
+          name: 'Expr',
+          expansion: {
+            kind: ExpansionKind.Alt,
+            alts: ['Atom', 'Sum', 'Prod'],
+          },
+        },
+        Atom: {
+          name: 'Atom',
+          expansion: {
+            kind: ExpansionKind.Alt,
+            alts: ['Name', 'Num'],
+          },
+        },
+        Sum: {
+          name: 'Sum',
+          expansion: {
+            kind: ExpansionKind.Seq,
+            seq: ['Expr', 'Expr'],
+          },
+        },
+        Prod: {
+          name: 'Prod',
+          expansion: {
+            kind: ExpansionKind.Seq,
+            seq: ['Expr', 'Expr'],
+          },
+        },
+        Name: {
+          name: 'Name',
+          expansion: {
+            kind: ExpansionKind.Terminal,
+            pattern: /^\w[\w\d]*$/,
+          },
+        },
+        Num: {
+          name: 'Num',
+          expansion: {
+            kind: ExpansionKind.Terminal,
+            pattern: /^\d+$/,
+          },
+        },
+      },
+    });
+  });
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { GrammarBuilder as Gb } from './grammar-builder';
-
-const arith = new Gb()
-  .addProduction('Equality', Gb.Seq('Expr', 'Expr'))
-  .addProduction('Expr', Gb.Alt('Lit', 'Sum', 'Prod', 'Diff'))
-  .addProduction('Lit', Gb.Alt('Num', 'Name'))
-  .addProduction('Num', Gb.Terminal(/^\d+$/))
-  .addProduction('Name', Gb.Terminal(/^\w[\w\d]*$/))
-  .addProduction('Sum', Gb.Seq('Expr', 'Expr'))
-  .addProduction('Prod', Gb.Seq('Expr', 'Expr'))
-  .addProduction('Diff', Gb.Seq('Expr', 'Expr'))
-  .startWith('Equality')
-  .build();
 
 ReactDOM.render(
   <React.StrictMode>
-    <pre>{JSON.stringify(arith, null, 2)}</pre>
+    <h1>Test</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
