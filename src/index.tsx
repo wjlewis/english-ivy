6d138d630dff8029bb4e375ee8ef56f32a7c3a12
wjl <william.lewis112358@gmail.com>
2020-12-28 11:14:39 -0500

    Tidy, and remove `checkTree` fn
    I may reimplement this at some point, but it is not important now.

--- src/index.tsx
+++ src/index.tsx
@@ -1,95 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { GrammarBuilder as Gb } from './grammar-builder';
-import { TreeKind } from './types/tree';
-import { checkTree } from './check-tree';
-
-const gram = new Gb()
-  .addProduction('Derivs', Gb.Star('Deriv'))
-  .addProduction('Deriv', Gb.Seq('Prems', 'Expr'))
-  .addProduction('Prems', Gb.Star('Deriv'))
-  .addProduction('Expr', Gb.Terminal(/^[a-zA-Z]+$/))
-  .startWith('Derivs')
-  .build();
-
-const tree = {
-  kind: TreeKind.Inner,
-  type: 'Derivs',
-  children: [
-    {
-      kind: TreeKind.Inner,
-      type: 'Deriv',
-      children: [
-        {
-          kind: TreeKind.Inner,
-          type: 'Prems',
-          children: [],
-        },
-        {
-          kind: TreeKind.Leaf,
-          type: 'Expr',
-          content: 'test',
-        },
-      ],
-    },
-    {
-      kind: TreeKind.Inner,
-      type: 'Deriv',
-      children: [
-        {
-          kind: TreeKind.Inner,
-          type: 'Prems',
-          children: [
-            {
-              kind: TreeKind.Inner,
-              type: 'Deriv',
-              children: [
-                {
-                  kind: TreeKind.Inner,
-                  type: 'Prems',
-                  children: [],
-                },
-                {
-                  kind: TreeKind.Leaf,
-                  type: 'Expr',
-                  content: 'A',
-                },
-              ],
-            },
-            {
-              kind: TreeKind.Inner,
-              type: 'Deriv',
-              children: [
-                {
-                  kind: TreeKind.Inner,
-                  type: 'Prems',
-                  children: [],
-                },
-                {
-                  kind: TreeKind.Leaf,
-                  type: 'Expr',
-                  content: 'C',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          kind: TreeKind.Leaf,
-          type: 'Expr',
-          content: 'B',
-        },
-      ],
-    },
-  ],
-};
-
-checkTree(tree as any, gram);
 
 ReactDOM.render(
   <React.StrictMode>
     <h1>Test</h1>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
